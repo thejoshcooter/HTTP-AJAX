1ba14c2d6ca56b4dc2040fbe899fd8834a4b90b5
josh cooter <thejoshcooter@gmail.com>
2019-05-08 15:15:17 -0500

    get data. pass data to state. add FriendsList component files.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    }
+  };
+
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/friends')
+    .then(res => this.setState({ friends: res.data }) )
+    .catch(err => console.log(err));
+  };
+  
   render() {
+    console.log(this.state.friends);
     return (
       <div>hello from new app</div>
     );
